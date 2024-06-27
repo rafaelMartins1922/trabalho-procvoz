d370a3363061db727ba3f596ff8da4c0f3707da8
rafaelmartins-proof <rafael.martins@proof.com.br>
2024-06-27 03:32:44 -0300

    fix: reduced code in file where model is tested from mic

--- test_model_from_mic.py
+++ test_model_from_mic.py
@@ -1,61 +1,22 @@
-import sounddevice as sd
-from scipy.io.wavfile import write
-import numpy as np
-import queue
-import os
+from utils.audio_recorder import AudioRecorder
+import tensorflow as tf
+from utils.spectrogram_generator import SpectrogramGenerator
 
-# Define a function to record audio continuously and save it as a .wav file after detecting speech
-def record_audio_continuous(sample_rate=44100, silence_threshold=0.01):
-    # Create a queue to hold the incoming audio chunks
-    q = queue.Queue()
+while True:
+    recorder = AudioRecorder()
+    specgen = SpectrogramGenerator()
 
-    # Initialize variables to track silence
-    silent_chunks = 0
-    silence_limit = 50  # Convert silence duration to number of samples
-    count = 0
+    recorder.record_audio_continuous()
 
-    # Specify the directory to save the .wav file
-    current_directory = os.getcwd()
+    x = 'recorded_input/recording.wav'
+    x = tf.io.read_file(str(x))
+    x, sample_rate = tf.audio.decode_wav(x, desired_channels=1, desired_samples=16000,)
+    x = tf.squeeze(x, axis=-1)
+    waveform = x
+    x = specgen.get_spectrogram(x)
+    x = x[tf.newaxis,...]
 
-    # Callback function to stream audio chunks into the queue
-    def callback(indata, frames, time, status):
-        if status:
-            print(status)
-        q.put(indata.copy())
-
-    # Open a new stream for recording
-    with sd.InputStream(samplerate=sample_rate, channels=2, callback=callback):
-        print("Recording started. Speak into the microphone.")
-        try:
-            # Create an empty list to hold the audio data
-            audio_data = []
-            while True:
-                # Retrieve audio chunk from the queue
-                chunk = q.get()
-                # Append chunk to the list
-                audio_data.append(chunk)
-                # Check if the chunk is silent
-                if np.abs(chunk).mean() < silence_threshold:
-                    silent_chunks += 1
-                else:
-                    silent_chunks = 0
-                # If silence is detected for longer than the silence limit, stop recording
-                if silent_chunks > silence_limit:
-                    # Concatenate all audio chunks
-                    audio_data = np.concatenate(audio_data, axis=0)
-                    # Save the recording as a .wav file
-                    filename = f"{current_directory}/recorded_input/recording{count}.wav"
-                    write( filename, sample_rate, audio_data)
-                    print(f"Recording saved as {filename}")
-                    silent_chunks = 0
-                    audio_data = []
-                    count+=1
-                    
-                print(silent_chunks)
-
-        except KeyboardInterrupt:
-            # Stop recording on Ctrl+C
-            print("Recording stopped by user.")
-
-# Record the audio continuously and save it as a .wav file after detecting speech
-record_audio_continuous()
+    imported = tf.saved_model.load("saved_model")
+    results = imported(waveform[tf.newaxis, :])
+    command = results['class_names'].numpy()[0].decode('utf-8')
+    print(command)
